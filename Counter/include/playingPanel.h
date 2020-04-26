beba2edffeb2fc77247d00ebfbc62f015f458bd1
neulii <neulii@gmx.at>
2020-04-26 12:23:28 +0200

    added playingpanel

---
+++ Counter/include/playingPanel.h
@@ -0,0 +1,24 @@
+#ifndef PLAYINGPANEL_H 
+#define PLAYINGPANEL_H 
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+
+
+class PlayingPanel : public wxPanel
+{
+
+private:
+
+       
+public:
+    PlayingPanel( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 250,64 ), long style = wxTAB_TRAVERSAL, const wxString& name = wxEmptyString );
+
+
+
+};
+
+#endif //PLAYINGPANEL 
