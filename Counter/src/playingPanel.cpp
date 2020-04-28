74017a5818987d4086eaeea7d62c2cd6e038efbc
neulii <neulii@gmx.at>
2020-04-28 20:17:25 +0200

    wird schon

--- Counter/src/playingPanel.cpp
+++ Counter/src/playingPanel.cpp
@@ -4,11 +4,18 @@
 PlayingPanel::PlayingPanel( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name ) 
 	: wxPanel( parent, id, pos, size, style, name )
 {
+	wxBoxSizer* playPanelSizer;
+	playPanelSizer = new wxBoxSizer(wxVERTICAL);
+
 
 
 	this->SetSize(parent->GetSize());
 
 	pointInput = new PointInputPanel(this);
+	//sizer->Add(playerName, 0, wxALL | wxEXPAND, 5);
+
+	playPanelSizer->Add(pointInput , wxALL | wxEXPAND,5);
+
 
 	//this->Layout();
 }
